e80f17bbb82044ddc03a2adf0e7ac920bb551bd0
IFFyArt <10446007@ntub.edu.tw>
2018-11-01 18:06:10 +0800

    feat: add hot spot

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
   $('.sidenav').sidenav();
+  $('.modal').modal();
   $('nav .name').on('click', () => {
     $('.action-list').toggleClass('show')
     $('.person .name').toggleClass('action')
