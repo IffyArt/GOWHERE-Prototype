48206b577eb968648e23d9369595902a35ad3cc1
IFFyArt <10446007@ntub.edu.tw>
2018-11-02 01:45:04 +0800

    feat: system-setting complate

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
   $('.sidenav').sidenav();
   $('.modal').modal();
+  $('select').formSelect();
   $('nav .name').on('click', () => {
     $('.action-list').toggleClass('show')
     $('.person .name').toggleClass('action')
