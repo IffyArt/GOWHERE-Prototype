fc5becfebadc1194bb2c589fef2468e89408ea0d
IFFyArt <10446007@ntub.edu.tw>
2018-11-03 22:45:48 +0800

    feat: expand

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -2,6 +2,10 @@ $(document).ready(function () {
   $('.sidenav').sidenav();
   $('.modal').modal();
   $('select').formSelect();
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
   $('nav .name').on('click', () => {
     $('.action-list').toggleClass('show')
     $('.person .name').toggleClass('action')
